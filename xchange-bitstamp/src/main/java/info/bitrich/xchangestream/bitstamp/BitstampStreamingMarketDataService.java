8a77f4c99994aa5dec982b1c7929166ecb184bc6
Wei Hong <physhunter@gmail.com>
2017-04-22 21:02:48 -0400

    Chang getTrades() to call the live_trades endpoint instead of live_orders

--- xchange-bitstamp/src/main/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataService.java
+++ xchange-bitstamp/src/main/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataService.java
@@ -13,8 +13,6 @@ import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
-
-import java.util.Arrays;
 import java.util.Date;
 
 public class BitstampStreamingMarketDataService implements StreamingMarketDataService {
@@ -47,14 +45,13 @@ public class BitstampStreamingMarketDataService implements StreamingMarketDataSe
 
     @Override
     public Observable<Trade> getTrades(CurrencyPair currencyPair, Object... args) {
-        String channelName = "live_orders" + getChannelPostfix(currencyPair);
+        String channelName = "live_trades" + getChannelPostfix(currencyPair);
 
-        return service.subscribeChannel(channelName, Arrays.asList("order_created", "order_changed", "order_deleted"))
+        return service.subscribeChannel(channelName, "trade")
                 .map(s -> {
                     ObjectMapper mapper = new ObjectMapper();
                     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                     BitstampWebSocketTransaction transactions = mapper.readValue(s, BitstampWebSocketTransaction.class);
-
                     return BitstampAdapters.adaptTrade(transactions, currencyPair, 1000);
                 });
     }
