cae5c77c57da8c2323da70a75d3d8e394ad11bef
pchertalev <pchertalev@gmail.com>
2018-03-21 11:39:13 +0300

    Ticker unit test

--- xchange-hitbtc/src/test/java/info/bitrich/xchangestream/hitbtc/HitbtcStreamingMarketDataServiceTest.java
+++ xchange-hitbtc/src/test/java/info/bitrich/xchangestream/hitbtc/HitbtcStreamingMarketDataServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.mockito.Mock;
@@ -19,7 +20,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -35,16 +35,14 @@ public class HitbtcStreamingMarketDataServiceTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         marketDataService = new HitbtcStreamingMarketDataService(streamingService);
     }
 
     @Test
     public void testOrderbookCommon() throws Exception {
 
-        Supplier<TestObserver<OrderBook>> updater = () -> marketDataService.getOrderBook(CurrencyPair.BTC_EUR).test();
-
-        // Given order book in JSON
+        // Read order book in JSON
         String orderBook = IOUtils.toString(getClass().getResource("/example/notificationSnapshotOrderBook.json"), "UTF8");
 
         when(streamingService.subscribeChannel(eq("orderbook-BTCEUR"))).thenReturn(Observable.just(objectMapper.readTree(orderBook)));
@@ -60,7 +58,7 @@ public class HitbtcStreamingMarketDataServiceTest {
         asks.add(new LimitOrder(Order.OrderType.ASK, new BigDecimal("2.784"), CurrencyPair.BTC_EUR, null, null, new BigDecimal("0.054591")));
 
         // Call get order book observable
-        TestObserver<OrderBook> test = updater.get();
+        TestObserver<OrderBook> test = marketDataService.getOrderBook(CurrencyPair.BTC_EUR).test();
 
         // We get order book object in correct order
         test.assertValue(orderBook1 -> {
@@ -72,7 +70,7 @@ public class HitbtcStreamingMarketDataServiceTest {
 
     @Test
     public void testGetTrades() throws Exception {
-        // Given order book in JSON
+        // Read trades in JSON
         String trades = IOUtils.toString(getClass().getResource("/example/notificationSnapshotTrades.json"), "UTF8");
 
         when(streamingService.subscribeChannel(eq("trades-BTCUSD"))).thenReturn(Observable.just(objectMapper.readTree(trades)));
@@ -81,15 +79,53 @@ public class HitbtcStreamingMarketDataServiceTest {
         Trade expected2 = new Trade(Order.OrderType.BID, new BigDecimal("0.092"), CurrencyPair.BTC_USD, new BigDecimal("0.054656"), new Date(1508430828754L), "54469497");
         Trade expected3 = new Trade(Order.OrderType.BID, new BigDecimal("0.002"), CurrencyPair.BTC_USD, new BigDecimal("0.054669"), new Date(1508430853288L), "54469697");
 
-        // Call get order book observable
+        // Call get trades observable
         TestObserver<Trade> test = marketDataService.getTrades(CurrencyPair.BTC_USD).test();
 
         test.assertValues(expected1, expected2, expected3);
+        validateTrade(0, test, expected1);
+        validateTrade(1, test, expected2);
+        validateTrade(2, test, expected3);
+    }
+
+    private void validateTrade(int index, TestObserver<Trade> test, Trade expected) {
+        test.assertValueAt(index, trade -> {
+            assertThat(trade.getPrice()).isEqualTo(expected.getPrice());
+            assertThat(trade.getType()).isEqualTo(expected.getType());
+            assertThat(trade.getOriginalAmount()).isEqualTo(expected.getOriginalAmount());
+            assertThat(trade.getCurrencyPair()).isEqualTo(expected.getCurrencyPair());
+            assertThat(trade.getTimestamp()).isEqualTo(expected.getTimestamp());
+            return true;
+        });
     }
 
     @Test
     public void testGetTicker() throws Exception {
-        //TODO: implement
-        //marketDataService.getTicker(CurrencyPair.BTC_EUR).test();
+        // Read ticker in JSON
+        String tickerString = IOUtils.toString(getClass().getResource("/example/notificationTicker.json"), "UTF8");
+
+        when(streamingService.subscribeChannel(eq("ticker-BTCUSD"))).thenReturn(Observable.just(objectMapper.readTree(tickerString)));
+
+        Ticker expected =
+                new Ticker.Builder()
+                        .currencyPair(CurrencyPair.BTC_USD).last(new BigDecimal("0.054463"))
+                        .bid(new BigDecimal("0.054463")).ask(new BigDecimal("0.054464"))
+                        .high(new BigDecimal("0.057559")).low(new BigDecimal("0.053615"))
+                        .volume(new BigDecimal("33068.346")).timestamp(new Date(1508427944941L))
+                        .build();
+
+        // Call get ticker observable
+        TestObserver<Ticker> test = marketDataService.getTicker(CurrencyPair.BTC_USD).test();
+
+        test.assertValue(ticker -> {
+            assertThat(ticker.getAsk()).isEqualTo(expected.getAsk());
+            assertThat(ticker.getBid()).isEqualTo(expected.getBid());
+            assertThat(ticker.getHigh()).isEqualTo(expected.getHigh());
+            assertThat(ticker.getLow()).isEqualTo(expected.getLow());
+            assertThat(ticker.getLast()).isEqualTo(expected.getLast());
+            assertThat(ticker.getVolume()).isEqualTo(expected.getVolume());
+            assertThat(ticker.getTimestamp()).isEqualTo(expected.getTimestamp());
+            return true;
+        });
     }
 }
