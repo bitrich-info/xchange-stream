e39887cdcc00793fb7ee939fc741bed80e1f7f86
Sergey Lukashevich <sergey.lukashevi4@gmail.com>
2018-04-17 18:00:21 +0300

    [Cexio] Fix wrong partial fill parsing
    Use a real format that exchange returns, not a wrong one that described in API documentation.

--- xchange-cexio/src/test/java/info/bitrich/xchangestream/cexio/CexioStreamingServiceTest.java
+++ xchange-cexio/src/test/java/info/bitrich/xchangestream/cexio/CexioStreamingServiceTest.java
@@ -27,7 +27,7 @@ public class CexioStreamingServiceTest {
     public void testGetOrderExecution_orderPlace() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("order-place.json"));
+                .getResourceAsStream("order-place.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
@@ -37,8 +37,8 @@ public class CexioStreamingServiceTest {
         service.handleMessage(jsonNode);
 
         CexioOrder expected = new CexioOrder(Order.OrderType.BID, CurrencyPair.BTC_USD, new BigDecimal("0.002"),
-                                             "5913254239", new Date(1522135708956L), new BigDecimal("7176.5"),
-                                             new BigDecimal("0.16"), Order.OrderStatus.NEW);
+                "5913254239", new Date(1522135708956L), new BigDecimal("7176.5"),
+                new BigDecimal("0.16"), Order.OrderStatus.NEW);
         test.assertValue(expected);
     }
 
@@ -46,7 +46,7 @@ public class CexioStreamingServiceTest {
     public void testGetOrderExecution_orderFill() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("order-fill.json"));
+                .getResourceAsStream("order-fill.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
@@ -64,7 +64,7 @@ public class CexioStreamingServiceTest {
     public void testGetOrderExecution_orderPartialFill() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("order-partial-fill.json"));
+                .getResourceAsStream("order-partial-fill.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
@@ -74,10 +74,10 @@ public class CexioStreamingServiceTest {
 
         service.handleMessage(jsonNode);
 
-        CexioOrder expected = new CexioOrder(CurrencyPair.BTC_USD,
-                                             "5891752542",
-                                             Order.OrderStatus.PARTIALLY_FILLED,
-                                             new BigDecimal("0.002"));
+        CexioOrder expected = new CexioOrder(Order.OrderType.ASK, CurrencyPair.BTC_USD, new BigDecimal("1.91342713"),
+                "6035463456", new Date(1523973448227L), new BigDecimal("782"),
+                new BigDecimal("0.15"), Order.OrderStatus.PARTIALLY_FILLED);
+
         test.assertValue(expected);
     }
 
@@ -85,7 +85,7 @@ public class CexioStreamingServiceTest {
     public void testGetOrderExecution_orderCancel() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("order-cancel.json"));
+                .getResourceAsStream("order-cancel.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
@@ -96,9 +96,9 @@ public class CexioStreamingServiceTest {
         service.handleMessage(jsonNode);
 
         CexioOrder expected = new CexioOrder(CurrencyPair.BTC_USD,
-                                             "5891717811",
-                                             Order.OrderStatus.CANCELED,
-                                             new BigDecimal("0.002"));
+                "5891717811",
+                Order.OrderStatus.CANCELED,
+                new BigDecimal("0.002"));
         test.assertValue(expected);
     }
 
@@ -119,7 +119,7 @@ public class CexioStreamingServiceTest {
     public void testGetTransaction_orderPlace() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("transaction-place.json"));
+                .getResourceAsStream("transaction-place.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
@@ -156,7 +156,7 @@ public class CexioStreamingServiceTest {
     public void testGetTransaction_orderExecute() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
-                                                             .getResourceAsStream("transaction-exec.json"));
+                .getResourceAsStream("transaction-exec.json"));
 
         CexioStreamingPrivateDataRawService service =
                 (CexioStreamingPrivateDataRawService) cexioStreamingExchange.getStreamingPrivateDataService();
