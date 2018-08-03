389aa11e0804cd10dbc9214f5bc86b98be1103b4
makarid <makarid@gmail.com>
2018-08-03 20:42:27 +0300

    CoinmateStream adding id on trade websocket response(BUY and SELL)

--- xchange-coinmate/src/main/java/info/bitrich/xchange/coinmate/dto/CoinmateWebSocketTrade.java
+++ xchange-coinmate/src/main/java/info/bitrich/xchange/coinmate/dto/CoinmateWebSocketTrade.java
@@ -9,18 +9,24 @@ public class CoinmateWebSocketTrade {
     private final long timestamp;
     private final BigDecimal price;
     private final BigDecimal amount;
+    private final String buyOrderId;
+    private final String sellOrderId;
 
     public CoinmateWebSocketTrade(
             @JsonProperty("date") long timestamp,
             @JsonProperty("price") BigDecimal price,
-            @JsonProperty("amount") BigDecimal amount
+            @JsonProperty("amount") BigDecimal amount,
+            @JsonProperty("buyOrderId") String buyOrderId,
+            @JsonProperty("sellOrderId") String sellOrderId
     ) {
         this.timestamp = timestamp;
         this.price = price;
         this.amount = amount;
+        this.buyOrderId = buyOrderId;
+        this.sellOrderId = sellOrderId;
     }
 
     public CoinmateTransactionsEntry toTransactionEntry(String currencyPair) {
-        return new CoinmateTransactionsEntry(timestamp, null, price, amount, currencyPair);
+        return new CoinmateTransactionsEntry(timestamp, "BUY"+buyOrderId+"SELL"+sellOrderId, price, amount, currencyPair);
     }
 }
