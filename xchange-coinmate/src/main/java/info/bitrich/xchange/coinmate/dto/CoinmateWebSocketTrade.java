fc88c76cdaf1ef8f0cb8b91e4869708052347d25
makarid <makarid@gmail.com>
2018-08-03 19:09:00 +0300

    Coinmate test again

--- xchange-coinmate/src/main/java/info/bitrich/xchange/coinmate/dto/CoinmateWebSocketTrade.java
+++ xchange-coinmate/src/main/java/info/bitrich/xchange/coinmate/dto/CoinmateWebSocketTrade.java
@@ -9,18 +9,15 @@ public class CoinmateWebSocketTrade {
     private final long timestamp;
     private final BigDecimal price;
     private final BigDecimal amount;
-    private final String id;
 
     public CoinmateWebSocketTrade(
             @JsonProperty("date") long timestamp,
             @JsonProperty("price") BigDecimal price,
-            @JsonProperty("amount") BigDecimal amount,
-            @JsonProperty("id") String id
+            @JsonProperty("amount") BigDecimal amount
     ) {
         this.timestamp = timestamp;
         this.price = price;
         this.amount = amount;
-        this.id = id;
     }
 
     public CoinmateTransactionsEntry toTransactionEntry(String currencyPair) {
