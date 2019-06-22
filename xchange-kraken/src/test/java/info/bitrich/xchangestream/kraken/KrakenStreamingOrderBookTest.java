c9fb5a43f90896a79e005c6af2cb0af007f4017b
makarid <makarid@gmail.com>
2019-06-22 19:35:00 +0300

    Minor fixes in coinmate and kraken API

--- xchange-kraken/src/test/java/info/bitrich/xchangestream/kraken/KrakenStreamingOrderBookTest.java
+++ xchange-kraken/src/test/java/info/bitrich/xchangestream/kraken/KrakenStreamingOrderBookTest.java
@@ -5,11 +5,17 @@ import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
 import io.reactivex.disposables.Disposable;
 import org.junit.Test;
+import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class KrakenStreamingOrderBookTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KrakenStreamingOrderBookTest.class);
 
     @Test
     public void orderBookTest() throws InterruptedException{
@@ -34,6 +40,31 @@ public class KrakenStreamingOrderBookTest {
             TimeUnit.SECONDS.sleep(10);
             dis.dispose();
             dis2.dispose();
+    }
+
+//    @Test
+    public void orderBookVerificationTest(){
+        StreamingExchange krakenExchange = StreamingExchangeFactory.INSTANCE.createExchange(KrakenStreamingExchange.class.getName());
+        krakenExchange.connect(ProductSubscription.create().addAll(CurrencyPair.ETH_BTC).addAll(CurrencyPair.BTC_USD).addAll(CurrencyPair.BTC_EUR).build()).blockingAwait();
+
+        krakenExchange.getStreamingMarketDataService().getOrderBook(CurrencyPair.BTC_EUR).subscribe(orderBook -> {
+            LOGGER.info("Ask 3: "+orderBook.getAsks().get(2).getLimitPrice()+" volume "+ orderBook.getAsks().get(2).getOriginalAmount());
+            LOGGER.info("Ask 2: "+orderBook.getAsks().get(1).getLimitPrice()+" volume "+ orderBook.getAsks().get(1).getOriginalAmount());
+            LOGGER.info("Ask 1: "+orderBook.getAsks().get(0).getLimitPrice()+" volume "+ orderBook.getAsks().get(0).getOriginalAmount());
+            LOGGER.info("--");
+            LOGGER.info("Bid 1: "+orderBook.getBids().get(0).getLimitPrice()+" volume "+ orderBook.getBids().get(0).getOriginalAmount());
+            LOGGER.info("Bid 2: "+orderBook.getBids().get(1).getLimitPrice()+" volume "+ orderBook.getBids().get(1).getOriginalAmount());
+            LOGGER.info("Bid 3: "+orderBook.getBids().get(2).getLimitPrice()+" volume "+ orderBook.getBids().get(2).getOriginalAmount());
+            LOGGER.info("=================");
+        });
+
+        while(true){
+            try{
+                TimeUnit.SECONDS.sleep(10000);
+            }catch (Exception e){
+                LOGGER.error(e.getMessage(),e);
+            }
+        }
     }
 
 }
