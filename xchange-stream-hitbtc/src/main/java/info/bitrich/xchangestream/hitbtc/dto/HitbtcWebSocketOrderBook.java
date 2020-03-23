d173251d07de242ff8124429c04e8be2df5daa11
David Peisakhov <dima@pesahov.net>
2020-03-23 01:25:28 +0000

    Issue #561: [HitBTC] OrderBook is missing support of timestamp

--- xchange-stream-hitbtc/src/main/java/info/bitrich/xchangestream/hitbtc/dto/HitbtcWebSocketOrderBook.java
+++ xchange-stream-hitbtc/src/main/java/info/bitrich/xchangestream/hitbtc/dto/HitbtcWebSocketOrderBook.java
@@ -3,6 +3,11 @@ package info.bitrich.xchangestream.hitbtc.dto;
 import static java.util.Collections.reverseOrder;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 import org.knowm.xchange.hitbtc.v2.dto.HitbtcOrderBook;
@@ -10,9 +15,13 @@ import org.knowm.xchange.hitbtc.v2.dto.HitbtcOrderLimit;
 
 /** Created by Pavel Chertalev on 15.03.2018. */
 public class HitbtcWebSocketOrderBook {
+  // To parse dates in format of '2018-11-19T05:00:28.700Z'
+  private static DateTimeFormatter DATE_FORMAT =
+          new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").toFormatter();
   private Map<BigDecimal, HitbtcOrderLimit> asks;
   private Map<BigDecimal, HitbtcOrderLimit> bids;
   private long sequence = 0;
+  private long timestamp = 0;
 
   public HitbtcWebSocketOrderBook(HitbtcWebSocketOrderBookTransaction orderbookTransaction) {
     createFromLevels(orderbookTransaction);
@@ -35,16 +44,22 @@ public class HitbtcWebSocketOrderBook {
     }
 
     sequence = orderbookTransaction.getParams().getSequence();
+    timestamp = parseISOTimestampInUTC(orderbookTransaction.getParams().getTimestamp());
+  }
+
+  private long parseISOTimestampInUTC(String timestamp) {
+    if (timestamp != null) {
+        LocalDateTime time = LocalDateTime.parse(timestamp, DATE_FORMAT);
+        return time.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+    return 0L;
   }
 
   public HitbtcOrderBook toHitbtcOrderBook() {
-    HitbtcOrderLimit[] askLimits =
-        asks.entrySet().stream().map(Map.Entry::getValue).toArray(HitbtcOrderLimit[]::new);
+    HitbtcOrderLimit[] asks = this.asks.values().toArray(new HitbtcOrderLimit[0]);
+    HitbtcOrderLimit[] bids = this.bids.values().toArray(new HitbtcOrderLimit[0]);
 
-    HitbtcOrderLimit[] bidLimits =
-        bids.entrySet().stream().map(Map.Entry::getValue).toArray(HitbtcOrderLimit[]::new);
-
-    return new HitbtcOrderBook(askLimits, bidLimits);
+    return new HitbtcOrderBookWithTimestamp(new Date(timestamp), asks, bids);
   }
 
   public void updateOrderBook(HitbtcWebSocketOrderBookTransaction orderBookTransaction) {
@@ -54,6 +69,7 @@ public class HitbtcWebSocketOrderBook {
     updateOrderBookItems(orderBookTransaction.getParams().getAsk(), asks);
     updateOrderBookItems(orderBookTransaction.getParams().getBid(), bids);
     sequence = orderBookTransaction.getParams().getSequence();
+    timestamp = parseISOTimestampInUTC(orderBookTransaction.getParams().getTimestamp());
   }
 
   private void updateOrderBookItems(
